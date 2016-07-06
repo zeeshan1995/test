1b2d9c152175ceedc557d6a9f8e6926d6c97e68e
Zeeshan Akhter <zeeshanakhter1729@gmail.com>
2016-07-06 10:29:51 +0530

    Test

---
+++ test.cpp
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+
+int add(int a ,int b)
+{
+	return 1;
+}
+int mul(int a ,int b)
+{
+	return 1;
+}
+int main()
+{
+	int a,b;
+	std::cin>>a>>b;
+
+	std::cout<<add(a,b)<<"\n":
+	std::cout<<mul(a,b)<<"\n":
+}
